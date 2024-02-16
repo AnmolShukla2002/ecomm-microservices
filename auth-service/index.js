fe80915d241b4ed5d2822f253a1046f402406192
Anmol Shukla <anmol.28422@gmail.com>
2024-02-16 22:51:45 +0530

    complete auth microservice

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require("./User");
 const app = express();
 const PORT = process.env.PORT_ONE || 7070;
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
 
 mongoose
   .connect("mongodb://localhost/auth-service")
@@ -15,4 +19,43 @@ mongoose
     console.error("Error connecting to MongoDB:", err.message);
   });
 
-app.use(express.json());
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: "User doesn't exists." });
+    } else {
+      if (password !== user.password) {
+        return res.json({ message: "Password Incorrect" });
+      }
+      const payload = {
+        email,
+        name: user.name,
+      };
+      jwt.sign(payload, "secret", (err, token) => {
+        if (err) {
+          console.log(err);
+        } else {
+          return res.json({ token: token });
+        }
+      });
+    }
+  } catch (error) {}
+});
+
+app.post("/auth/register", async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.json({ message: "User Already Exists" });
+    } else {
+      const newUser = new User({ name, email, password });
+      newUser.save();
+      return res.json(newUser);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
